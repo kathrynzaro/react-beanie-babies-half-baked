1ec2425bf9625700ae424cb3c5904fb4825bc1d6
kathrynzaro <zarokathryn@gmail.com>
2022-06-22 14:44:05 -0700

    displaying beanies list, pagination

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  NavLink
 } from 'react-router-dom';
 import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
@@ -11,11 +12,20 @@ export default function App() {
   return (
     <Router>
       <div className='App'>
+        <div>
+          <ul>
+            <li>
+              <NavLink exact activeClassName='active-link' to="/">List Page</NavLink>
+            </li>
+          </ul>
+        </div>
         <Switch>
-          <Route>
+          <Route exact path="/">
+            <BeaniesPage />
             {/* this home page route should list all the beanies */}
           </Route>
-          <Route> 
+          <Route exact path="/beanies/:id"> 
+            <BeanieDetail />
             {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
           </Route>
         </Switch>
--- src/BeaniesList.js
+++ src/BeaniesList.js
@@ -4,5 +4,12 @@ export default function BeaniesList({ beanieBabies }) {
   return (
     <div className='beanies'>
       {/* render out the beanie babies as a list */}
+      {
+        beanieBabies.map((beanieBaby, i) => {
+          return <BeanieBaby
+            {...beanieBaby}
+            key={beanieBaby.title + i + beanieBaby.id} beanieBaby={beanieBaby} />;
+        })
+      }
     </div>);
 }
